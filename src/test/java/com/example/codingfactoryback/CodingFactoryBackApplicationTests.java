feafd46069bc4c0ba629e95ac53f6ba6e7c803a6
salahoumaya <salah.oumaya@esprit.tn>
2025-02-16 11:21:19 +0100

    Initial commit: Ajout du projet Spring Boot

---
+++ src/test/java/com/example/codingfactoryback/CodingFactoryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingfactoryback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingFactoryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
