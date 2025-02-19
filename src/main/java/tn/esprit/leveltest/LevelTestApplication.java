e8c07d586673593aa1d34b32811a786932346011
chmaryem <mariemchalghoumi6666@gmail.com>
2025-02-19 13:52:08 +0100

    Initial commit for TestlevelManagement

---
+++ src/main/java/tn/esprit/leveltest/LevelTestApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.leveltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/leveltest/LevelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.leveltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
