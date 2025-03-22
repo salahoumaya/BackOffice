3168cc3e0a88ebfc4df7c5b7ac53615becf8354e
Arij-BenAlaya <Arij.BenAlaya@esprit.tn>
2025-03-22 17:59:42 +0100

    update Training Entity

--- src/main/java/com/userPI/usersmanagementsystem/entity/Training.java
+++ src/main/java/com/userPI/usersmanagementsystem/entity/Training.java
@@ -1,11 +1,18 @@
 package com.userPI.usersmanagementsystem.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.userPI.usersmanagementsystem.entity.user.OurUsers;
 import jakarta.persistence.*;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Entity
@@ -31,6 +38,45 @@ public class Training {
 
     @Enumerated(EnumType.STRING)
     private typeTraining typeTraning;
+
+
+    @OneToMany(mappedBy = "training", cascade = CascadeType.ALL)
+    private List<Course> courses = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "training_users",
+            joinColumns = @JoinColumn(name = "training_id"),
+            inverseJoinColumns = @JoinColumn(name = "users_id")
+    )
+    private List<OurUsers> users = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "training_users", joinColumns = @JoinColumn(name = "training_id"))
+    @MapKeyColumn(name = "users_id")
+    @Column(name = "subscription_date")
+    private Map<Integer, LocalDateTime> subscriptionDates = new HashMap<>();
+
+    @JsonIgnore
+    @ManyToOne
+    private OurUsers trainer;
+
+    @OneToMany(mappedBy = "training", cascade = CascadeType.ALL)
+    private List<Planning> plannings = new ArrayList<>();
+
+    public Map<Integer, LocalDateTime> getSubscriptionDates() {
+        return subscriptionDates == null ? new HashMap<>() : subscriptionDates;
+    }
+
+    public void addUserToTraining(OurUsers user) {
+        if (users == null) {
+            users = new ArrayList<>();
+        }
+        if (!users.contains(user)) {
+            users.add(user);
+            subscriptionDates.put(user.getId(), LocalDateTime.now());
+        }
+    }
 
 
 }
