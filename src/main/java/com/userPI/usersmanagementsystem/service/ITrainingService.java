ca43d404bd293d9f4395bbd12ad7f85e18639197
Arij-BenAlaya <Arij.BenAlaya@esprit.tn>
2025-04-02 20:28:50 +0100

    Add Services Training and Courses

---
+++ src/main/java/com/userPI/usersmanagementsystem/service/ITrainingService.java
@@ -0,0 +1,25 @@
+package com.userPI.usersmanagementsystem.service;
+
+
+//import com.userPI.usersmanagementsystem.dto.dashModerator;
+import com.userPI.usersmanagementsystem.entity.Training;
+
+import java.util.List;
+
+public interface ITrainingService {
+
+
+    public List<Training> getAllTrainings();
+    public Training getTrainingById(int id);
+    public Training addTraining(Training training);
+    public Training addTrainingWithCourses(Training training);
+    public void deleteTrainingWithCourses(int id);
+    public Training updateTrainingWithCourses(int id, Training newTrainingData);
+    public Training addUserToTraining(int trainingId);
+    List<Training> getTrainingsForAuthenticatedTrainer();
+    List<Training> getTrainingsForAuthenticatedStudent();
+    //dashModerator getDashboard();
+
+
+
+}
