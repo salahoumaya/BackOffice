1f77d1f680814ab92c9355fa6b84f58da3ddfc97
Arij-BenAlaya <Arij.BenAlaya@esprit.tn>
2025-04-02 17:30:41 +0100

    add Repositories

---
+++ src/main/java/com/userPI/usersmanagementsystem/repository/PlanningRepository.java
@@ -0,0 +1,16 @@
+package com.userPI.usersmanagementsystem.repository;
+
+import com.phegondev.usersmanagementsystem.entity.Planning;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface PlanningRepository extends JpaRepository<Planning, Integer> {
+
+    @Query("SELECT p FROM Planning p WHERE FUNCTION('DATE', p.startDate) = FUNCTION('DATE', :startDate)")
+    List<Planning> findByStartDate(Date startDate);
+}
---
+++ src/main/java/com/userPI/usersmanagementsystem/repository/RevokedTokenRepo.java
@@ -0,0 +1,10 @@
+package com.userPI.usersmanagementsystem.repository;
+
+import com.phegondev.usersmanagementsystem.entity.RevokedToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RevokedTokenRepo extends JpaRepository<RevokedToken, Integer> {
+    Optional<RevokedToken> findByToken(String token);
+}
